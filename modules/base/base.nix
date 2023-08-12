853539680871f93a7e66665f9d586fda1fa78178
h1k0n <ybzjyhk@gmail.com>
2023-08-12 16:49:38 +0800

    use my publicKey

--- modules/base/base.nix
+++ modules/base/base.nix
@@ -82,7 +82,6 @@
         pyyaml
 
         numpy
-        opencv4
       ]
     ))
   ];
--- modules/base/user-group.nix
+++ modules/base/user-group.nix
@@ -4,7 +4,7 @@ let
   # this is the hash of the password "lp4a"
   hashedPassword = "$y$j9T$mTIe/80yqh53danHYbUkP1$2TYCUsFKnkBnHG6pArOv9t.e2ofxO.j1MIt/6rB05P1";
   # TODO replace this with your own public key!
-  publickey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK3F3AH/vKnA2vxl72h67fcxhIK8l+7F/bdE1zmtwTVU ryan@romantic";
+  publickey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMkX9M2A+OwRYDoDvH8wj6U535JsLtmx7hswSCzz3CJo ha1kang@VivoBook";
 in {
   # =========================================================================
   #      Users & Groups NixOS Configuration
